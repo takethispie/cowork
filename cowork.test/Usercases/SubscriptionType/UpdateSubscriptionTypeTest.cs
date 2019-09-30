973f33bf535204b1e36e31a6be2af9f2bf3dda7e
Alexandre Felix <takethispie@gmail.com>
2019-09-30 21:23:12 +0200

    add subscription type tests

---
+++ cowork.test/Usercases/SubscriptionType/UpdateSubscriptionTypeTest.cs
@@ -0,0 +1,33 @@
+﻿using cowork.domain.Interfaces;
+using cowork.usecases.SubscriptionType;
+using Moq;
+using NUnit.Framework;
+
+namespace cowork.test.Usercases.SubscriptionType {
+
+    [TestFixture]
+    public class UpdateSubscriptionTypeTest {
+
+        [Test]
+        public void ShouldUpdateSubscriptionType() {
+            var subType = new domain.SubscriptionType(0, "test", 8, 5, 3, 30, 25, 200, 280, "description");
+            var mockSubTypeRepo = new Mock<ISubscriptionTypeRepository>();
+            mockSubTypeRepo.Setup(m => m.Update(subType)).Returns(0);
+            
+            var res = new UpdateSubscriptionType(mockSubTypeRepo.Object, subType).Execute();
+            Assert.AreEqual(0, res);
+        }
+        
+        [Test]
+        public void ShouldFailUpdatingSubscriptionType() {
+            var subType = new domain.SubscriptionType(0, "test", 8, 5, 3, 30, 25, 200, 280, "description");
+            var mockSubTypeRepo = new Mock<ISubscriptionTypeRepository>();
+            mockSubTypeRepo.Setup(m => m.Update(It.IsAny<domain.SubscriptionType>())).Returns(-1);
+            
+            var res = new UpdateSubscriptionType(mockSubTypeRepo.Object, subType).Execute();
+            Assert.AreEqual(-1, res);
+        }
+
+    }
+
+}
