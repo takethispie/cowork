b109da4353ebec3e99a4db3de93e4f5d318805c0
Alexandre Felix <takethispie@gmail.com>
2019-10-02 20:41:15 +0200

    fix room booking not checking for overlaps

--- cowork.usecases/RoomBooking/CreateRoomBooking.cs
+++ cowork.usecases/RoomBooking/CreateRoomBooking.cs
@@ -22,17 +22,29 @@ namespace cowork.usecases.RoomBooking {
 
 
         public long Execute() {
+            if (Input.Start.Date != Input.End.Date) return -1;
             var placeId = roomRepository.GetById(Input.RoomId).PlaceId;
             var canBook = timeSlotRepository
                 .GetAllOfPlace(placeId)
-                .Any(slot => slot.Day == Input.Start.DayOfWeek
-                             && new TimeSpan(0, slot.EndHour, slot.EndMinutes, 0)
-                             >= new TimeSpan(0, Input.End.Hour, Input.End.Minute, 0)
-                             && new TimeSpan(0, slot.StartHour, slot.StartMinutes, 0)
-                             <= new TimeSpan(0, Input.Start.Hour, Input.Start.Minute, 0));
-            if (!canBook) throw new Exception("une réservation existe déjà pour cette plage horaire");
+                .Any(slot => IsOutsideOfOpeningTimes(slot));
+            if (!canBook) throw new Exception("Impossible de réserver en dehors des horaires d'ouverture");
+            var otherSlots = roomBookingRepository.GetAllFromGivenDate(Input.Start.Date);
+            if (otherSlots != null) {
+                var overlapping = otherSlots.Where(slot => slot.End >= Input.End && slot.Start <= Input.Start)
+                    .Any(slot => Input.RoomId == slot.RoomId || slot.ClientId == Input.ClientId);
+                if (overlapping) return -1;
+            }
             var roomBooking = new domain.RoomBooking(Input.Start, Input.End, Input.RoomId, Input.ClientId);
             return roomBookingRepository.Create(roomBooking);
+        }
+
+
+        private bool IsOutsideOfOpeningTimes(domain.TimeSlot slot) {
+            return slot.Day == Input.Start.DayOfWeek
+                   && new TimeSpan(0, slot.EndHour, slot.EndMinutes, 0)
+                   >= new TimeSpan(0, Input.End.Hour, Input.End.Minute, 0)
+                   && new TimeSpan(0, slot.StartHour, slot.StartMinutes, 0)
+                   <= new TimeSpan(0, Input.Start.Hour, Input.Start.Minute, 0);
         }
 
     }
--- cowork.usecases/RoomBooking/Models/CreateRoomBookingInput.cs
+++ cowork.usecases/RoomBooking/Models/CreateRoomBookingInput.cs
@@ -4,6 +4,15 @@ namespace cowork.usecases.RoomBooking.Models {
 
     public class CreateRoomBookingInput {
 
+        public CreateRoomBookingInput() { }
+
+        public CreateRoomBookingInput(DateTime start, DateTime end, long roomId, long clientId) {
+            Start = start;
+            End = end;
+            RoomId = roomId;
+            ClientId = clientId;
+        }
+
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public long RoomId { get; set; }
